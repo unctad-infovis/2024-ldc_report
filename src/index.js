f2f926b126a4a4b9cdcbab9144bd6a28f2f7c09f
Teemo Tebest <teemo.tebest@gmail.com>
2024-10-31 20:17:34 +0100

    Updated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { createRoot } from 'react-dom/client';
 
 import Footer from './jsx/Footer.jsx';
 
-const container_footer = document.getElementById('app-root-2024-rmt_report_footer');
+const container_footer = document.getElementById('app-root-2024-ldc_report_footer');
 const root_footer = createRoot(container_footer);
 root_footer.render(<Footer />);
