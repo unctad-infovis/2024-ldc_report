c6c585b5a88b31f5ce129afee5805a96db2c597d
Teemo Tebest <teemo.tebest@gmail.com>
2024-11-04 10:09:27 -0300

    Updated footer

--- src/jsx/Footer.jsx
+++ src/jsx/Footer.jsx
@@ -15,11 +15,11 @@ function Footer() {
         <div className="footer_elements">
           <div className="footer_element footer_element_1">
             <div className="footer_content anchor_videos" id="anchor_videos">
-              <h3>Watch the videos</h3>
+              {/* <h3>Watch the videos</h3>
               <div className="iframe_container youtube_iframe">
                 <iframe src="https://www.youtube.com/embed/PLzwchtLJSk" title="Least Developed Countries Report 2024" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-              </div>
-              <ul>
+              </div> */}
+              {/* <ul>
                 <li>
                   <a href="#download" target="_blank" rel="noreferrer">Français</a>
                   {', '}
@@ -41,8 +41,8 @@ function Footer() {
                   {', '}
                   <a href="#download" target="_blank" rel="noreferrer">Deutsch</a>
                 </li>
-              </ul>
-              <h4>Download the report video</h4>
+              </ul> */}
+              {/* <h4>Download the report video</h4>
               <ul>
                 <li>
                   <a href="https://vimeo.com/1022047383" target="_blank" rel="noreferrer">English</a>
@@ -67,12 +67,12 @@ function Footer() {
                   {', '}
                   <a href="#download" target="_blank" rel="noreferrer">Deutsch</a>
                 </li>
-              </ul>
-              <h4>Watch the press conference</h4>
+              </ul> */}
+              {/* <h4>Watch the press conference</h4>
               <div className="iframe_container youtube_iframe">
                 <iframe src="https://www.youtube.com/embed/AYD39tO7VGM" title="Least Developed Countries Report 2024 press conference" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-              </div>
-              <h4>Media assets</h4>
+              </div> */}
+              {/* <h4>Media assets</h4>
               <ul>
                 <li>
                   <a href="#download" target="_blank" rel="noreferrer">Press conference</a>
@@ -83,14 +83,14 @@ function Footer() {
                 <li>
                   <a href="#download" target="_blank" rel="noreferrer">Audio and other assets</a>
                 </li>
-              </ul>
+              </ul> */}
             </div>
             <div className="footer_content anchor_podcasts" id="anchor_podcasts">
               <h3>Podcasts</h3>
               <p>Listen to the Weekly Tradecast episodes that explore some of the main issues in the report</p>
               <div className="iframe_container">
-                <iframe title="96. Rough seas ahead: How can global supply chains navigate shipping disruptions?" height="150" width="100%" style={{ border: 'none', minWidth: 'min(100%, 430px)' }} scrolling="no" data-name="pb-iframe-player" src="https://www.podbean.com/player-v2/?i=zix3p-161db2f-pb&btn-skin=009EDB&download=1&font-color=000000&fonts=Verdana&from=pb6admin&logo_link=none&rtl=0&share=1&size=240&skin=ffffff" allowFullScreen />
-                <span className="text"><a href="/podcast/rough-seas-ahead-how-can-global-supply-chains-navigate-shipping-disruptions">Rough seas ahead: How can global supply chains navigate shipping disruptions</a></span>
+                <iframe title="120. Trading up: How carbon markets can help development and the planet" height="150" width="100%" style={{ border: 'none', minWidth: 'min(100%, 430px)' }} scrolling="no" data-name="pb-iframe-player" src="https://www.podbean.com/player-v2/?i=chbvn-172ad41-pb&btn-skin=009EDB&download=1&font-color=000000&fonts=Verdana&from=pb6admin&logo_link=none&rtl=0&share=1&size=240&skin=ffffff" allowFullScreen />
+                <span className="text"><a href="/podcast/trading-how-carbon-markets-can-help-development-and-planet">Trading up: How carbon markets can help development and the planet</a></span>
               </div>
             </div>
           </div>
